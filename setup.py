36f8fb9507d1ad523224d7d7f841bcfda8f2c345
Aleksandar Gotev <alex@igenius.net>
2017-09-15 18:30:29 +0200

    added basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lokalise-exporter',
+      version='0.1',
+      description='Utility tool to export localization strings from lokalise.co for iOS/Android/Frontend/Backend',
+      url='https://github.com/gotev/lokalise-exporter',
+      author='Aleksandar Gotev',
+      author_email='alexgotev@gmail.com',
+      license='Apache-2.0',
+      packages=['lokalise-exporter'],
+      zip_safe=False)
