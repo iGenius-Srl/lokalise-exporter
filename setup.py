90547d253b8157cbda98b5c74d0eedb9b8eb9653
Aleksandar Gotev <alex@igenius.net>
2017-09-18 17:35:58 +0200

    correctly setup python script entry point

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lokalise-exporter',
-      version='0.1',
+      version='0.4',
       description='Export strings from lokalise.co',
       long_description='Utility tool to export localization strings from lokalise.co for iOS/Android/Frontend/Backend',
       url='https://github.com/gotev/lokalise-exporter',
@@ -9,7 +9,7 @@ setup(name='lokalise-exporter',
       author_email='alexgotev@gmail.com',
       license='Apache-2.0',
       classifiers=[
-          'Development Status :: 1 - Planning',
+          'Development Status :: 3 - Alpha',
           'Environment :: Console',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
@@ -19,7 +19,11 @@ setup(name='lokalise-exporter',
           'Topic :: Software Development :: Localization'
       ],
       keywords='localization l10n internationalization i18n lokalise localise export exporter tools development',
-      packages=['lokalise-exporter'],
+      packages=['lokalise_exporter'],
+      entry_points={
+          # This has to be like this because of begins package for command line parsing
+          'console_scripts': ['lokalise-exporter=lokalise_exporter.main:main.start'],
+      },
       install_requires=['requests', 'begins', 'colorlog'],
       python_requires='>=3',
       zip_safe=False)
